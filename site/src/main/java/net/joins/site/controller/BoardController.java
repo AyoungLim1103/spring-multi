de05bd0f4cbdc459ce62b168a2aeb674b171e0e6
LAPTOP-5BSLTEQ8\Joins <lim.ayoung@joins.com>
2020-11-29 22:05:37 +0900

    2020.11.29 임아영 controller수정

--- site/src/main/java/net/joins/site/controller/BoardController.java
+++ site/src/main/java/net/joins/site/controller/BoardController.java
@@ -7,24 +7,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import net.joins.domain.service.BoardService;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+@RequiredArgsConstructor
 @Controller
 @Log
 @RequestMapping("/boards/")
 public class BoardController {
+
+    final BoardService boardService;
 
     @GetMapping("/list")
     public void list(Model model){
         log.info("list() called...");
         List<BoardInfo> listBoardInfo = new ArrayList<>();
         for(long i=1L; i<11L; i++){
-            listBoardInfo.add(new BoardInfo(i,"title"+i,i/3,"username"+i,"content"+i,"N",new Timestamp(System.currentTimeMillis()),new Timestamp(System.currentTimeMillis())));
+         //   listBoardInfo.add(new BoardInfo(i,"title"+i,i/3,"username"+i,"content"+i,"N",new Timestamp(System.currentTimeMillis()),new Timestamp(System.currentTimeMillis())));
         }
 
+        listBoardInfo = boardService.getListAll();
         model.addAttribute("listBoardInfo", listBoardInfo);
     }
 
