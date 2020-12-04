3fc237f2bef43bf2cb961779a1573eb0771cdf6d
a-ri-mu <arumu215@gmail.com>
2020-12-05 01:10:52 +0900

    20.12.05 김아름 게시판 CRUD 완료

--- site/src/main/java/net/joins/site/controller/BoardController.java
+++ site/src/main/java/net/joins/site/controller/BoardController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import net.joins.domain.service.BoardService;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.sql.Timestamp;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Controller
 @Log
@@ -42,10 +45,13 @@ public class BoardController {
     public void registerGET(@ModelAttribute("vo") BoardInfo vo,
                             @ModelAttribute("mvo") MemberInfo mvo){
         log.info("register get");
+
+        //추후 로그인한 Member의 정보로 넣기
+        mvo.setMemberSeq(1L);
+        mvo.setName("user01");
+        vo.setMemberInfo(mvo);
+
         log.info("vo : "+vo);
-       // mvo.setMemberSeq(1L);
-       //mvo.setMemberId("user01");
-        log.info("mvo : "+mvo);
     }
 
     @PostMapping("/register")
@@ -53,15 +59,68 @@ public class BoardController {
                                @ModelAttribute("mvo") MemberInfo mvo,
                                RedirectAttributes rttr){
         log.info("register post");
-        log.info("vo : "+vo);
+        //추후 로그인한 Member의 정보로 넣기
         mvo.setMemberSeq(1L);
         vo.setMemberInfo(mvo);
-        log.info("mvo : "+mvo);
-
-        //MemberInfo memeberInfo = new MemberInfo(); error
+        log.info("vo : "+vo);
 
         boardService.saveContent(vo);
         rttr.addFlashAttribute("msg", "success");
+
+        return "redirect:/boards/list";
+    }
+
+    @GetMapping("/view")
+    public void viewGET(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model){
+        log.info("BNO : "+bno);
+
+        //Optional<T> 복잡한 조건문 없이도 널(null) 값으로 인해 발생하는 예외를 처리
+        //Optional 객체가 감싸고 있는 값이 존재할 경우에만, 실행될 로직을 함수형 인자로 넘김
+        boardService.getContent(bno).ifPresent(board ->model.addAttribute("vo",board));
+    }
+
+    @GetMapping("/modify")
+    public void modifyGET(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model){
+        log.info("MODIFY BNO : "+bno);
+
+        boardService.modifyContent(bno).ifPresent(board ->model.addAttribute("vo",board));
+    }
+
+    @PostMapping("/modify")
+    public String modifyPOST(BoardInfo boardInfo, PageVO vo, RedirectAttributes rttr){
+        log.info("MODIFY BoardInfo : "+boardInfo);
+
+        boardService.modifyContent(boardInfo.getBno()).ifPresent(origin -> {
+            origin.setTitle(boardInfo.getTitle());
+            origin.setContent(boardInfo.getContent());
+            origin.setUpdatedate(new Timestamp(System.currentTimeMillis())); //수정한 시간
+
+            boardService.saveContent(origin);
+            rttr.addFlashAttribute("msg","success");
+            rttr.addAttribute("bno",origin.getBno());
+        });
+
+        rttr.addAttribute("page",vo.getPage());
+        rttr.addAttribute("size",vo.getSize());
+        rttr.addAttribute("type",vo.getType());
+        rttr.addAttribute("keyword",vo.getKeyword());
+
+        return "redirect:/boards/list";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Long bno, @ModelAttribute("pageVO") PageVO vo, RedirectAttributes rttr){
+        log.info("MODIFY BNO : "+bno);
+
+        boardService.deleteContent(bno);
+
+        rttr.addFlashAttribute("msg","success");
+
+        //list로 이동
+        rttr.addAttribute("page",vo.getPage());
+        rttr.addAttribute("size",vo.getSize());
+        rttr.addAttribute("type",vo.getType());
+        rttr.addAttribute("keyword",vo.getKeyword());
 
         return "redirect:/boards/list";
     }
