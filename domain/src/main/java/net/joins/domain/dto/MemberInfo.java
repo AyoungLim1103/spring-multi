d350b29a8b027f88cbc366cf04ebbb417ebb8cd8
a-ri-mu <arumu215@gmail.com>
2020-12-04 09:30:58 +0900

    20.12.04 김아름 저장 완료 -- 기본 USERSEQ 1 로 설정, 후에 해당부분 보충 예정

--- domain/src/main/java/net/joins/domain/dto/MemberInfo.java
+++ domain/src/main/java/net/joins/domain/dto/MemberInfo.java
@@ -15,6 +15,7 @@ import java.util.List;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 public class MemberInfo {
     Long memberSeq;
     String memberId;
--- site/src/main/java/net/joins/site/controller/BoardController.java
+++ site/src/main/java/net/joins/site/controller/BoardController.java
@@ -6,6 +6,7 @@ import net.joins.domain.dto.BoardInfo;
 import net.joins.domain.dto.MemberInfo;
 import net.joins.domain.vo.PageMaker;
 import net.joins.domain.vo.PageVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
@@ -38,15 +39,26 @@ public class BoardController {
     }
 
     @GetMapping("/register")
-    public void registerGET(@ModelAttribute("vo") BoardInfo vo){
+    public void registerGET(@ModelAttribute("vo") BoardInfo vo,
+                            @ModelAttribute("mvo") MemberInfo mvo){
         log.info("register get");
+        log.info("vo : "+vo);
+       // mvo.setMemberSeq(1L);
+       //mvo.setMemberId("user01");
+        log.info("mvo : "+mvo);
     }
 
     @PostMapping("/register")
     public String registerPOST(@ModelAttribute("vo") BoardInfo vo,
+                               @ModelAttribute("mvo") MemberInfo mvo,
                                RedirectAttributes rttr){
         log.info("register post");
         log.info("vo : "+vo);
+        mvo.setMemberSeq(1L);
+        vo.setMemberInfo(mvo);
+        log.info("mvo : "+mvo);
+
+        //MemberInfo memeberInfo = new MemberInfo(); error
 
         boardService.saveContent(vo);
         rttr.addFlashAttribute("msg", "success");
