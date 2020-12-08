02d810729c798fef0a3e33566a1b766f8b09db83
a-ri-mu <arumu215@gmail.com>
2020-12-08 23:21:11 +0900

    20.12.08 김아름 로그인 관련 화면 및 8.3 완성

--- site/src/main/java/net/joins/site/security/SecurityConfig.java
+++ site/src/main/java/net/joins/site/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package net.joins.site.security;
 
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,8 +11,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    protected void configure(HttpSecurity http) throws Exception{
-        log.info("security debug...");
+    protected void configure(HttpSecurity http) throws Exception {
+        log.info("security config...");
+
+        http.authorizeRequests().antMatchers("/guest/**").permitAll(); //모든 사용자 접근 가능
+        http.authorizeRequests().antMatchers("/manager/**").hasRole("MANAGER"); //특정 권한자 접근 가능
+        http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN"); //특정 권한자 접근 가능
+
+        // http.formLogin(); //시큐리티에서 자동으로 만들어주는 로그인 페이지
+        http.formLogin().loginPage("/login"); //커스텀 로그인 페이지
+
+        http.exceptionHandling().accessDeniedPage("/accessDenied");
+
+        //logout
+        //http.logout().invalidateHttpSession(true);
+        http.logout().logoutUrl("/logout").invalidateHttpSession(true);
+    }
+
+    @Autowired
+    public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception{
+        log.info("Build Auth global......");
+
+        auth.inMemoryAuthentication()
+                .withUser("manager")
+                .password("{noop}1111")
+                .roles("MANAGER");
+
     }
 
 }
