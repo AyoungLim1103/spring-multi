1626b2cdd496c14cf533647e0ca914ab46522c17
LAPTOP-5BSLTEQ8\Joins <lim.ayoung@joins.com>
2020-12-08 18:07:26 +0900

    2020.12.08 임아영 SiteUser 추가

---
+++ site/src/main/java/net/joins/site/security/SiteUsersService.java
@@ -0,0 +1,31 @@
+package net.joins.site.security;
+
+import lombok.extern.java.Log;
+import net.joins.domain.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+
+@Service
+@Log
+public class SiteUsersService implements UserDetailsService {
+
+    @Autowired
+    MemberRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User sampleUser = new User(username, "1111", Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")));
+        //return repo.findMemberByMemberId(username).filter(m -> m != null).map(m -> new SiteSecurityUser(m)).get();
+
+        return sampleUser;
+    }
+}
