0e16995048e5f040c14fa0ab4546cccb7a5ec1e5
LAPTOP-5BSLTEQ8\Joins <lim.ayoung@joins.com>
2020-12-09 17:22:15 +0900

    2020.12.09 임아영 MemberRepository 추가 및 thymeleaf-extras-springsecurity4 추가 (thmeleaf 에서 스프링 시큐리티 사용 위함)

--- domain/src/main/java/net/joins/domain/repository/MemberRepository.java
+++ domain/src/main/java/net/joins/domain/repository/MemberRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long> {
+
+    Member findMemberByMemberId(String memberId);
 }
--- site/src/main/java/net/joins/site/security/SiteUsersService.java
+++ site/src/main/java/net/joins/site/security/SiteUsersService.java
@@ -1,6 +1,7 @@
 package net.joins.site.security;
 
 import lombok.extern.java.Log;
+import net.joins.domain.entity.Member;
 import net.joins.domain.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -23,9 +24,8 @@ public class SiteUsersService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        User sampleUser = new User(username, "{noop}1111", Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")));
-        //return repo.findMemberByMemberId(username).filter(m -> m != null).map(m -> new SiteSecurityUser(m)).get();
-
-        return sampleUser;
+        //User sampleUser = new User(username, "{noop}1111", Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")));
+        Member member = repo.findMemberByMemberId(username);
+        return repo.findById(member.getMemberSeq()).filter(m -> m != null).map(m -> new SiteSecurityUser(m)).get();
     }
 }
