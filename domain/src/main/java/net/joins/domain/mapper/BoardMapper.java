4b1f48991b7a9f9871ec8baa1e6971ee84c684c4
LAPTOP-5BSLTEQ8\Joins <lim.ayoung@joins.com>
2020-12-21 16:05:58 +0900

    2020.12.21 임아영 BoardMapper 수정 (무한loop 현상 해소)

--- domain/src/main/java/net/joins/domain/mapper/BoardMapper.java
+++ domain/src/main/java/net/joins/domain/mapper/BoardMapper.java
@@ -8,20 +8,17 @@ import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(uses = {MemberMapper.class, ReplyMapper.class})
+@Mapper(uses = {MemberMapper.class})
 public interface BoardMapper {
     BoardMapper INSTANCE = Mappers.getMapper(BoardMapper.class);
 
     @Mapping(source = "board.member", target = "memberInfo")
-    @Mapping(source = "board.replies", target = "repliesInfo")
     BoardInfo boardToBoardInfo(Board board);
 
     @Mapping(source = "boardInfo.memberInfo", target = "member")
-    @Mapping(source = "boardInfo.repliesInfo", target = "replies")
     Board boardInfoToBoard(BoardInfo boardInfo);
 
     @Mapping(source = "boards.member", target = "memberInfo")
-    @Mapping(source = "boards.replies", target = "repliesInfo")
     List<BoardInfo> boardsToBoardInfos(List<Board> boards);
 
 }
