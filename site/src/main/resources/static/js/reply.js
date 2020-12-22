a6b9b87925e4654bdad24197751cfdac07239ccd
a-ri-mu <arumu215@gmail.com>
2020-12-22 17:38:17 +0900

    20.12.22 김아름 reply.js 수정,삭제,저장 등 추가

--- site/src/main/resources/static/js/reply.js
+++ site/src/main/resources/static/js/reply.js
@@ -7,14 +7,41 @@ var replyManager = (function () {
 
     var add = function (obj,callback) {
         console.log("add....");
+
+        $.ajax({
+           type:'post',
+           utl:'/replies/'+obj.bno,
+           data:JSON.stringify(obj),
+           dataType:'json',
+           contentType:"application/json" ,
+            success:callback
+        });
     };
 
     var update = function (obj, callback) {
         console.log("update....");
+
+        $.ajax({
+            type:'put',
+            utl:'/replies/'+obj.bno,
+            data:JSON.stringify(obj),
+            dataType:'json',
+            contentType:"application/json" ,
+            success:callback
+        });
     };
 
     var remove = function (obj, callback) {
         console.log("remove...");
+
+        $.ajax({
+            type:'delete',
+            utl:'/replies/'+obj.bno+"/"+obj.rno,
+            data:JSON.stringify(obj),
+            dataType:'json',
+            contentType:"application/json" ,
+            success:callback
+        });
     };
 
     return{
